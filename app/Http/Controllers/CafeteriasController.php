f3960f5a049a0895e5a2c14ef78b49b46057efd7
Ruben Vargas <ruben.vargas@vanadys.com>
2024-02-22 18:36:11 +0100

    Controlles ADD

---
+++ app/Http/Controllers/CafeteriasController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cafeterias;
+use App\Http\Requests\StoreCafeteriasRequest;
+use App\Http\Requests\UpdateCafeteriasRequest;
+
+class CafeteriasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCafeteriasRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cafeterias $cafeterias)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cafeterias $cafeterias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCafeteriasRequest $request, Cafeterias $cafeterias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cafeterias $cafeterias)
+    {
+        //
+    }
+}
